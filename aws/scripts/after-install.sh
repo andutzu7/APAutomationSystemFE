342c0e7fb3611168e8f71af42e15154764d8b95a
Gabriela Ursachi <gabriela.ursachi@basware.com>
2023-09-21 14:13:05 +0300

    Another pipeline test

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,6 +3,6 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://frontendstack-angularappdeploymentbucket-ls6xy05xzn8v/apafinance-fe/ /home/ec2-user/dist/apafinance-fe/
+aws s3 sync s3://frontendstack-angularappdeploymentbucket-ls6xy05xzn8v/apafinance-fe/ /home/ec2-user/dist/apafinance-fe/
 
 stack_name="FrontendStack"
