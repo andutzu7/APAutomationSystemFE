1645789691dc7e000fbf87b2c26ba62cec7d4ceb
Gabriela Ursachi <gabriela.ursachi@basware.com>
2023-09-21 11:45:05 +0300

    Pipeline test

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,6 +3,6 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp --recursive s3://frontendstack-angularappdeploymentbucket-zx4kl57mv874/dist /home/ec2-user/
+aws s3 cp --recursive s3://frontendstack-angularappdeploymentbucket-zx4kl57mv874/apafinance-fe /home/ec2-user/dist/apafinance-fe/
 
 stack_name="FrontendStack"
