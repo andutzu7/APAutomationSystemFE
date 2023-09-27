ae451a2b94514c6ebe0a02711a93d91fd77a6c70
Gabriela Ursachi <gabriela.ursachi@basware.com>
2023-09-27 10:30:44 +0300

    Changed bucket's name

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -xe
 
-aws s3 sync s3://frontendstack-angularappdeploymentbucket-1344cf8sn9lqm/apafinance-fe/ /home/ec2-user/dist/apafinance-fe/
+aws s3 sync s3://frontendstack-angularappdeploymentbucket-19wcdbkyy8e9j/apafinance-fe/ /home/ec2-user/dist/apafinance-fe/
