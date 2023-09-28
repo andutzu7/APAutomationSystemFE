e1007827835c94582bad1bfbd81edbb73cd29f8c
Gabriela Ursachi <gabriela.ursachi@basware.com>
2023-09-28 13:25:52 +0300

    Pipeline test

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -xe
 
-aws s3 sync s3://frontendstack-angularappdeploymentbucket-19wcdbkyy8e9j/apafinance-fe/ /home/ec2-user/dist/apafinance-fe/
+aws s3 sync s3://frontendstack-angularappdeploymentbucket-1itiy9r58udxs/apafinance-fe/ /home/ec2-user/dist/apafinance-fe/
