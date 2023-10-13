99df35ad3680a2a1b4bc047e374168c72e7cebf2
andutzu7 <andreivavilov@pseudot.org>
2023-10-13 14:23:39 +0300

    fixed error handler bug

--- src/assets/api-paths.ts
+++ src/assets/api-paths.ts
@@ -1,6 +1,6 @@
 export enum ApiPaths {
     //base = 'https://api.pseudot.org/api',
-    base = 'http://Backen-LoadB-0n06UbnQeXYU-2096691058.us-east-1.elb.amazonaws.com/api',
+    base = 'http://Backen-LoadB-trxy0ysamSy6-1916069205.us-east-1.elb.amazonaws.com/api',
     //base = 'http://localhost:8080/api',
     getInvoicesMapping = 'invoices',
     postInvoicesMapping = 'invoices',
