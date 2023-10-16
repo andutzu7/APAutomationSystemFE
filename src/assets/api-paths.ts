a4da96b9829c8b6c905dc8b3201edc91f91e1112
Gabriela Ursachi <gabriela.ursachi@basware.com>
2023-10-16 11:28:24 +0300

    Pipeline test

--- src/assets/api-paths.ts
+++ src/assets/api-paths.ts
@@ -1,6 +1,6 @@
 export enum ApiPaths {
     //base = 'https://api.pseudot.org/api',
-    base = 'http://Backen-LoadB-BFcqtia34TgC-990123420.us-east-1.elb.amazonaws.com/api',
+    base = 'http://Backen-LoadB-eMynOTMrbl6R-371617271.us-east-1.elb.amazonaws.com/api',
     //base = 'http://localhost:8080/api',
     getInvoicesMapping = 'invoices',
     postInvoicesMapping = 'invoices',
