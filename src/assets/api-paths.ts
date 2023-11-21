ac03cd6d75d635e2b4078c3e5b49a5f0d78649ea
Gabriela Ursachi <gabriela.ursachi@basware.com>
2023-11-21 11:29:59 +0200

    Changed ARNs of AWS resources for deploy

--- src/assets/api-paths.ts
+++ src/assets/api-paths.ts
@@ -1,7 +1,7 @@
 export enum ApiPaths {
     //base = 'https://api.pseudot.org/api',
-    // base = 'http://Backen-LoadB-PzUPwprA1U3e-1301876444.us-east-1.elb.amazonaws.com/api',
-    base = 'http://localhost:8080/api',
+    base = 'http://Backen-LoadB-xYjsauw15G83-1532516174.us-east-1.elb.amazonaws.com/api',
+    //base = 'http://localhost:8080/api',
     getInvoicesMapping = 'invoices',
     postInvoicesMapping = 'invoices',
     putInvoicesMapping = 'invoices',
