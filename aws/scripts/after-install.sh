a15836319132d9f628cd5b7fe2d6d31a5befa327
Gabriela Ursachi <gabriela.ursachi@basware.com>
2023-11-27 16:21:42 +0200

    Just changed ARNs of AS resources

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -xe
 
-aws s3 sync s3://frontendstack-angularappdeploymentbucket-dmhtezarmsvh/apafinance-fe/ /home/ec2-user/dist/apafinance-fe/
+aws s3 sync s3://frontendstack-angularappdeploymentbucket-73ts6y9o06by/apafinance-fe/ /home/ec2-user/dist/apafinance-fe/
