b14bebe47f29d233fa2850f3a844e12bfd005cfb
Andrei Vavilov <andrei.vavilov@basware.com>
2023-12-05 13:38:33 +0200

    pipeline trigger

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -xe
 
-aws s3 sync s3://frontendstack-angularappdeploymentbucket-aa0ierwtloqh/apafinance-fe/ /home/ec2-user/dist/apafinance-fe/
+aws s3 sync s3://frontendstack-angularappdeploymentbucket-1fbhrlfne8jfr/apafinance-fe/ /home/ec2-user/dist/apafinance-fe/
