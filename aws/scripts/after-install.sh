84cca84532143fc4c2ae20a9cff6d217aab03ab9
Andrei Vavilov <andrei.vavilov@basware.com>
2023-12-05 15:55:46 +0200

    pipeline trigger

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -xe
 
-aws s3 sync s3://frontendstack-angularappdeploymentbucket-1fbhrlfne8jfr/apafinance-fe/ /home/ec2-user/dist/apafinance-fe/
+aws s3 sync s3://frontendstack-angularappdeploymentbucket-1ldflklccsceq/apafinance-fe/ /home/ec2-user/dist/apafinance-fe/
--- src/assets/api-paths.ts
+++ src/assets/api-paths.ts
@@ -1,7 +1,7 @@
 export enum ApiPaths {
     //base = 'https://api.pseudot.org/api',
-    // base = 'http://Backen-LoadB-3zexyOu9ZTXA-1600671652.us-east-1.elb.amazonaws.com/api',
-    base = 'http://localhost:8080/api',
+    base = 'http://Backen-LoadB-mWlGXO0Rxld1-292198739.us-east-1.elb.amazonaws.com/api',
+    // base = 'http://localhost:8080/api',
     getInvoicesMapping = 'invoices',
     postInvoicesMapping = 'invoices',
     putInvoicesMapping = 'invoices',
