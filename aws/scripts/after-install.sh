ad0cd8a19a91f5c8d134096809558a86cd8ff026
Octavian Mocanu <octavmocanu13@gmail.com>
2023-12-14 10:49:48 +0200

    pipeline trigger

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -2,4 +2,4 @@
 set -xe
 
 
-aws s3 sync s3://frontendstack-angularappdeploymentbucket-kl3bri00c09f/apafinance-fe/ /home/ec2-user/dist/apafinance-fe/
+aws s3 sync s3://frontendstack-angularappdeploymentbucket-tpio9p46l4nc/apafinance-fe/ /home/ec2-user/dist/apafinance-fe/
