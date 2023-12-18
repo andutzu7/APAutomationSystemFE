409932f403220814cd9f338cd79308bfd32affe7
andutzu7 <andrei.vavilov@pseudot.org>
2023-12-18 10:40:48 +0200

    pipeline trigger

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -2,4 +2,4 @@
 set -xe
 
 
-aws s3 sync s3://frontendstack-angularappdeploymentbucket-kl3bri00c09f/apafinance-fe/ /home/ec2-user/dist/apafinance-fe/
+aws s3 sync s3://frontendstack-angularappdeploymentbucket-3cqumrxgxwtj/apafinance-fe/ /home/ec2-user/dist/apafinance-fe/
--- src/assets/api-paths.ts
+++ src/assets/api-paths.ts
@@ -1,7 +1,7 @@
 export enum ApiPaths {
-    //base = 'https://api.pseudot.org/api',
-    base = 'http://Backen-LoadB-B0FFprCSrIAT-2031513456.us-east-1.elb.amazonaws.com/api',
-    //base = 'http://localhost:8080/api',
+    base = 'https://api.pseudot.org/api',
+    // base = 'http://Backen-LoadB-8St9bQblQWNp-382557083.us-east-1.elb.amazonaws.com/api',
+    // base = 'http://localhost:8080/api',
     getInvoicesMapping = 'invoices',
     postInvoicesMapping = 'invoices',
     putInvoicesMapping = 'invoices',
